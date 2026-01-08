426a113cb65ab67423ec0357c2fe09b07a181010
huypham6688 <bilabu44@gmail.com>
2026-01-08 17:25:39 +0700

    update12

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,14 +1,8 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-    // A list of all locales that are supported
     locales: ["en", "vi"],
-
-    // Used when no locale matches
     defaultLocale: "en",
-
-    // THÊM DÒNG NÀY
-    localePrefix: "as-needed", // '/' sẽ dùng defaultLocale (en)
-
+    localePrefix: "always", // THÊM DÒNG NÀY
     localeDetection: true,
 });
